a40260975c1d9631f914eaa8628ff096b82be555
VictorKaue <victorkaue909@gmail.com>
2025-06-14 14:09:43 -0300

    rotas de posts criada no server.js

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+
+router.get('/', postController.getTodosPosts);
+router.get('/:id', postController.getPostPeloId);
+router.patch('/:id', postController.atualizarPost);
+router.post('/', postController.cadastrarPost);
+router.delete('/:id', postController.deletarPost);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const PORTA = 3000;
 const rotasUsuario = require('./routes/userRoutes')
-
+const rotasPost = require('./routes/postRoutes');
+const db = require('./config/connection');
 
 app.use(express.json());
 app.use('/api/usuario', rotasUsuario);
+app.use('/api/post', rotasPost);
 
+
+db.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao conectar ao banco de dados:', erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso');
+});
 
 app.listen(PORTA, () => {
     console.log(`Express rodando na porta ${PORTA}`);
